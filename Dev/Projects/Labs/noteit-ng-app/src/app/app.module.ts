3dfd64cbffcb479899b39554346a9dc121bf66a1
ssetiawan <Knightknight90>
2020-05-02 17:20:26 -0400

    routing works

--- Dev/Projects/Labs/noteit-ng-app/src/app/app.module.ts
+++ Dev/Projects/Labs/noteit-ng-app/src/app/app.module.ts
@@ -4,15 +4,43 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'notes',
+    component: NotesComponent
+  },
+  {
+    path: 'feedback',
+    component: FeedbackComponent
+  },
+  {
+    path: '',
+    component: NotesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
