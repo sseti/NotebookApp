4cadb42ef6f7573a8d4d546dd42eeb4c2d54916e
ssetiawan <Knightknight90>
2020-05-02 19:02:57 -0400

    first commit

---
+++ Dev/Projects/Labs/noteit-api/src/main/java/com/example/NoteBookApp/NoteBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.NoteBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteBookAppApplication.class, args);
+	}
+
+}
---
+++ Dev/Projects/Labs/noteit-api/src/test/java/com/example/NoteBookApp/NoteBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NoteBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
