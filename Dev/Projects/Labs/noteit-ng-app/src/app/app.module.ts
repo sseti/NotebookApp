63b5bf8916d6a67f4dc92321528fbbf060cb5e0c
ssetiawan <Knightknight90>
2020-05-05 14:49:55 -0400

    making a commit

--- Dev/Projects/Labs/noteit-ng-app/src/app/app.module.ts
+++ Dev/Projects/Labs/noteit-ng-app/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { NotesComponent } from './notes/notes.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import {Router, RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 const appRoutes: Routes = [
   {
@@ -39,7 +41,9 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
--- Dev/Projects/Labs/noteit-ng-app/src/app/notes/notes.component.ts
+++ Dev/Projects/Labs/noteit-ng-app/src/app/notes/notes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Notebook} from './model/notebook';
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
+  notebooks: Notebook[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
   }
 
+  ngOnInit() {
+    this.getAllNotebooks();
+  }
+
+  public getAllNotebooks() {
+    const url = 'http://localhost:8081/api/notebooks/all';
+    this.http.get<Notebook[]>(url).subscribe(
+      res => {
+        this.notebooks = res;
+      },
+      err => {
+        alert('An error has occured;')
+      }
+    );
+  }
 }
+
+
